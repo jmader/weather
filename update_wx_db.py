22c8ea84d4a9ac352350f598fd90a03c20daa9fb
Jeff Mader <jeff.mader@gmail.com>
2018-02-14 10:36:45 -1000

    Removed printing of result

--- update_wx_db.py
+++ update_wx_db.py
@@ -20,12 +20,10 @@ def updateWxDb(sendUrl, log_writer=''):
 	url = 'https://www.keck.hawaii.edu/software/db_api/koa.php?'
 
 	myHash = hashlib.md5(user.encode('utf-8')).hexdigest()
+	myHash = hashlib.md5(b'koaadmin').hexdigest()
 	sendUrl = ''.join((url, sendUrl, '&hash=', myHash))
 
 	if log_writer:
 		log_writer.info('weather.py {}'.format(sendUrl))
 
 	data = urllib.request.urlopen(sendUrl)
-	data = data.read().decode('utf8')       # Convert from byte to ascii
-	data = json.loads(data)                 # Convert to Python list
-	print(data)
